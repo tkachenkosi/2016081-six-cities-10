b660b311db2b56fb00fccaaf100bfbf2dfa5d0c4
Sergey Tkachenko <tkachenkosi@gmail.com>
2022-07-11 07:52:03 +0300

    Создание компонентов главных страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main/main';
+// import FavoritesScreen from '../../pages/favorites/favorites';
+// import PropertyScreen from '../../pages/property/property';
+// import LoginScreen from '../../pages/login/login';
+
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen offersCount = {offersCount} />
+  );
 }
 
 export default App;
