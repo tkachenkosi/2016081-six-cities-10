a23cf012cbbf3b40a6ff3ec1e35d1dae901506d8
Sergey Tkachenko <tkachenkosi@gmail.com>
2022-08-29 04:53:30 +0300

    перенес стили в одтдельный файл

--- project/src/pages/error404-screen/error404-screen.tsx
+++ project/src/pages/error404-screen/error404-screen.tsx
@@ -1,14 +1,15 @@
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../consts';
 import Logo from '../../components/logo/logo';
+import './error404-screen.css';
 
 function Error404Screen(): JSX.Element {
   return (
-    <section style={{backgroundColor:'#f0f0f0',textAlign:'center',fontFamily:'Tahoma'}}>
-      <div style={{margin:'5px 15px'}}><Logo /></div>
-      <h1 style={{color:'#c83349'}}>ОШИБКА 404</h1>
+    <section className="error-section">
+      <div className="error-logo"><Logo /></div>
+      <h1 className="error-title">ОШИБКА 404</h1>
       <h2>ЗАПРАШИВАЕМЫЙ РЕСУРС НЕ НАЙДЕН</h2>
-      <p style={{color:'blue'}}><Link to={AppRoute.Root}>Вернуться на главную страницу</Link></p>
+      <p className="error-link"><Link to={AppRoute.Root}>Вернуться на главную страницу</Link></p>
     </section>
   );
 }
